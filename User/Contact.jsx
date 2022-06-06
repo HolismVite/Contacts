8cd26645b28248b64574b2407f03852fba899543
Saeed Nemati <sn.1361@gmail.com>
2022-06-06 10:01:23 +0430

    committed by script

---
+++ User/Contact.jsx
@@ -0,0 +1,61 @@
+import InfoIcon from '@mui/icons-material/Info';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Tabs, Tab } from '@Tab'
+import { List, app, get, post } from '@List'
+import { Form, Text, Phone } from '@Form'
+
+const tabs = <>
+    <Tab
+        title="Info"
+        icon={InfoIcon}
+        panel={<Form
+            entityType='Profile'
+            inputs={<>
+                <Text
+                    column='FirstName'
+                    placeholder='First name'
+                />
+            </>}
+        />}
+    />
+    <Tab
+        title="Phones"
+        icon={LocalPhoneIcon}
+        panel={<List
+            entityType="Phone"
+            headers={<>
+                <th>Phone number</th>
+                <th>Country</th>
+            </>}
+            row={(item) => <>
+                <th>{item.number}</th>
+                <th>{item.country}</th>
+            </>}
+            create={<Form
+                entityType="Phone"
+                inputs={<>
+                    <Phone
+                        column="PhoneValue"
+                        placeholder="Number"
+                        required="You have not provided the phone number"
+                    />
+                </>}
+            />}
+        />}
+    />
+    <Tab
+        title="Addresses"
+        icon={LocationOnIcon}
+        panel={<div>Addresses</div>}
+    />
+</>
+
+const Contact = () => {
+    return <Tabs
+        title="Contact information"
+        tabs={tabs}
+    />
+}
+
+export default Contact
